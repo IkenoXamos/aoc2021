d3e32e85076cc2da1745452a1998657ec15fa9bf
IkenoXamos <moberlies1337@gmail.com>
2021-12-08 19:56:50 -0500

    Complete Day 8 Solution

--- src/day08/index.ts
+++ src/day08/index.ts
@@ -5,7 +5,7 @@ type Display = [input: string[], output: string[]]
 const parseInput = (rawInput: string): Display[] => rawInput.split('\n').map(line => line.split(' | ').map(part => part.split(' ')) as [string[], string[]]);
 
 function hasUniqueLength(part: string): number | undefined {
-  switch(part.length) {
+  switch (part.length) {
     case 2: return 1;
     case 3: return 7;
     case 4: return 4;
@@ -17,10 +17,10 @@ function hasUniqueLength(part: string): number | undefined {
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return input.reduce<number>((acc, line) => acc + 
-          line[1].reduce<number>((acc, part) => acc + (hasUniqueLength(part) ? 1 : 0),
-          0),
-  0);
+  return input.reduce<number>((acc, line) => acc +
+    line[1].reduce<number>((acc, part) => acc + (hasUniqueLength(part) ? 1 : 0),
+      0),
+    0);
 };
 
 // A grid representing the indices of letters (a = 0 ... g = 6)
@@ -40,22 +40,19 @@ const digit_wire_indices = [
 ]
 
 function deduce_invalid_mappings(connections: boolean[][], part: string, validIndexes: number[]): void {
-  const letterIndexesFromPart = Array.from(part).map( letter => letter.charCodeAt(0) - 97);
+  const letterIndexesFromPart = Array.from(part).map(letter => letter.charCodeAt(0) - 97);
 
-  for(let i = 0; i < 7; i++) {
-    if(!letterIndexesFromPart.includes(i)) {
+  for (let i = 0; i < 7; i++) {
+    if (!letterIndexesFromPart.includes(i)) {
       continue;
     }
 
-    for(let j = 0; j < 7; j++) {
-      if(validIndexes.includes(j)) {
+    for (let j = 0; j < 7; j++) {
+      if (validIndexes.includes(j)) {
         continue;
       }
-      console.log(`Analyzing ${part} -> ${validIndexes.map( val => String.fromCharCode(97 + val)).reduce<string>( (acc, letter) => acc + letter, '')}`);
-      console.log(`Deducing that ${String.fromCharCode(i + 97)} is not ${String.fromCharCode(j + 97)}`)
-      // console.log(`Deducing that ${i} is not ${j}`)
+      // console.log(` Deducing that ${String.fromCharCode(i + 97)} is not ${String.fromCharCode(j + 97)}`)
       connections[i][j] = false;
-      connections[j][i] = false;
     }
   }
 }
@@ -69,39 +66,41 @@ const part2 = (rawInput: string) => {
 
   const output: number[] = [];
 
-  for(const display of input) {
+  for (const display of input) {
 
     // A 7 x 7 grid that maps a - g against a - g, representing the possibility of the wire mismatch.
     // Each cell will start as true, and as certain combinations are filtered out, the cells will be set to false
     // In the end, each row should have a single true value, and the indices of those positions represent the wire connections
     const connections: boolean[][] = Array<boolean[]>(7);
-    for(let letter = 0; letter < connections.length; letter++) {
+    for (let letter = 0; letter < connections.length; letter++) {
       connections[letter] = Array<boolean>(7).fill(true);
     }
 
-    const intersections: { 532: string[], 960: string[] } = { 532: [], 960: []};
+    const intersections: { 532: string[], 960: string[] } = { 532: [], 960: [] };
 
     const [first, second] = display;
 
-    for(const part of first) {
+    for (const part of first) {
       const identifiedDigit = hasUniqueLength(part);
-      if(!identifiedDigit) {
+      if (!identifiedDigit) {
         // We need to track 2,3,5 and 0,6,9 separately and determine the intersection of signals
         // This will provide some additional mapping information
 
-        if(part.length === 5) intersections[532].push(part);
-        if(part.length === 6) intersections[960].push(part);
+        if (part.length === 5) intersections[532].push(part);
+        if (part.length === 6) intersections[960].push(part);
 
-        if(intersections[532].length === 3) {
-          const intersection = intersections[532].reduce<string>( (acc, letter) => (acc.match(new RegExp('[' + letter + ']', 'g')) || []).join(''), 'abcdefg');
+        if (intersections[532].length === 3) {
+          const intersection = intersections[532].reduce<string>((acc, letter) => (acc.match(new RegExp('[' + letter + ']', 'g')) || []).join(''), 'abcdefg');
 
-          // deduce_invalid_mappings(connections, intersection, [2, 3, 5]);
+          // console.log(`Analyzing ${intersection} -> ${[0, 3, 6].map(val => String.fromCharCode(97 + val)).reduce<string>((acc, letter) => acc + letter, '')}`);
+          deduce_invalid_mappings(connections, intersection, [0, 3, 6]);
         }
-  
-        if(intersections[960].length === 3) {
-          const intersection = intersections[960].reduce<string>( (acc, letter) => (acc.match(new RegExp('[' + letter + ']', 'g')) || []).join(''), 'abcdefg');
 
-          // deduce_invalid_mappings(connections, intersection, [0, 6, 9]);
+        if (intersections[960].length === 3) {
+          const intersection = intersections[960].reduce<string>((acc, letter) => (acc.match(new RegExp('[' + letter + ']', 'g')) || []).join(''), 'abcdefg');
+
+          // console.log(`Analyzing ${intersection} -> ${[0, 1, 5, 6].map(val => String.fromCharCode(97 + val)).reduce<string>((acc, letter) => acc + letter, '')}`);
+          deduce_invalid_mappings(connections, intersection, [0, 1, 5, 6]);
         }
 
         continue;
@@ -113,56 +112,49 @@ const part2 = (rawInput: string) => {
       // In this example, the identified digit would have been 4, which normally has connections 'bcdf'
       // This means that each of 'c', 'g', 'e', and 'b' can only have options of 'b', 'c', 'd', or 'f'
 
+      // console.log(`Analyzing ${part} -> ${digit_wire_indices[identifiedDigit].map(val => String.fromCharCode(97 + val)).reduce<string>((acc, letter) => acc + letter, '')}`);
       deduce_invalid_mappings(connections, part, digit_wire_indices[identifiedDigit]);
     }
 
-    const connectionsTranspose = transpose(connections);
+    while (connections.reduce<number>((acc, row) => acc + row.reduce((acc, value) => acc + (value ? 1 : 0), 0), 0) !== 7) {
+      for (let i = 0; i < connections.length; i++) {
+        const row = connections[i];
+        const rowCount = row.reduce((acc, value) => acc + (value ? 1 : 0), 0);
 
-    for(let i = 0; i < connections.length; i++) {
-      const row = connections[i];
-      const column = connectionsTranspose[i];
-      const rowCount = row.reduce( (acc, value) => acc + (value ? 1 : 0), 0);
-      const colCount = column.reduce( (acc, value) => acc + (value ? 1 : 0), 0);
+        if (rowCount === 1) {
+          const index = row.findIndex((value) => value);
+          // console.log(`Determined that row ${i} contains is a singleton at column ${index}`);
 
-      if(rowCount === 1) {
-        const index = row.findIndex( (value) => value);
-        console.log(`Determined that row ${i} contains is a singleton at column ${index}`);
-        console.table(connections);
-
-        // for(let j = 0; j < connections.length; j++) {
-        //   if(j === index) continue;
-        //   connections[index][j] = false;
-        // }
-      }
-
-      if(colCount === 1) {
-        const index = column.findIndex( (value) => value);
-
-        // for(let j = 0; j < connections.length; j++) {
-        //   if(j === index) continue;
-        //   connections[j][index] = false;
-        // }
+          for (let j = 0; j < connections.length; j++) {
+            if (j === i) continue;
+            connections[j][index] = false;
+          }
+        }
       }
     }
 
+    const wires = connections.reduce<number[]>((acc, row, index) => { acc[index] = row.findIndex(value => value); return acc; }, Array<number>(7).fill(0));
 
-    for(const row of connections) {
-      if(row.reduce( (acc, value) => acc + (value ? 1 : 0), 0) !== 1) {
-        console.table(connections);
-        console.log(connectionsTranspose[0]);
-        console.log(connections[2]);
-        throw new Error('We did not sufficiently deduce the wire mappings');
+    let value = '';
+    for (const part of second) {
+      const segments = Array.from(part).map(letter => letter.charCodeAt(0) - 97);
+
+      if (hasUniqueLength(part)) {
+        value += hasUniqueLength(part);
+        continue;
       }
+
+      const validSegments = segments.map((segment) => wires[segment]).sort();
+
+      const digit = digit_wire_indices.findIndex((sequence) => JSON.stringify(sequence) === JSON.stringify(validSegments));
+
+      value += String(digit);
     }
 
-    output.push(0);
+    output.push(Number(value));
   }
 
-  if(output.length !== input.length) {
-    throw new Error('Not correct amount of outputs');
-  }
-
-  return output.reduce( (acc, val) => acc + val);
+  return output.reduce((acc, val) => acc + val);
 };
 
 run({
